ac8e0163c2eab2f35dfc579902c04b1d49c6d4d7
rummolprod999 <rummolprod999@gmail.com>
2019-01-10 10:53:53 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tatsushid/go-fastping"
+	"net"
+	"time"
+)
+
+func main() {
+	for i := 0; i <= 255; i++ {
+		for j := 0; j <= 255; j++ {
+			for k := 0; k <= 255; k++ {
+				for l := 0; l <= 255; l++ {
+					ip := fmt.Sprintf("%d.%d.%d.%d", i, j, k, l)
+					pinger(ip)
+				}
+			}
+		}
+	}
+}
+
+func pinger(addr string) {
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr("ip4:icmp", addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
